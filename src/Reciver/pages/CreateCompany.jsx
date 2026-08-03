b767fea2fa9cdc1a407a1ed9f44bec5dabe6b95a
Shiva040912 <shivaarun0071@gmail.com>
2026-08-03 19:31:14 +0530

    fixed 3rd problem

--- src/Reciver/pages/CreateCompany.jsx
+++ src/Reciver/pages/CreateCompany.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import "../styles/createCompany.css";
+import "../styles/createcompany.css";
 
 const CreateCompany = () => {
   const navigate = useNavigate();
